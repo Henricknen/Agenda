f2b14039d35abb370665ca4e3fea9c430c9342cd
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-20 00:51:23 -0300

    criacao_da_classe_modelo_chamada_'Contato'_que_representa_os_dados_do_contato_que_serao_trabalhados_e_a_classe_'Down'_que_trabalhara_com_os_dados_do_'banco_de_dados'_sendo_responsavel_por_criar_o_banco_de_dados_inserir_listar_alterar_fazer_tudo_relacionado_ao_banco

---
+++ AgendaWF/Contato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaWF
+{
+    public class Contato {      // classe com modificador e acesso 'publc' onde todos podem ulilizar, classe 'Contato' representa um contato
+        public int Id { get; set; }
+
+        public int Nome { get; set; }       // propriedades
+
+        public int Email { get; set; }
+    }
+}
---
+++ AgendaWF/DALAgenda.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;       // importação da bliblioteca 'Data.SQLite'
+using System.Data;
+
+namespace AgendaWF
+{
+    public class DALAgenda {        // classe 'DALAgenda' ultiliza modificador de acesso 'public'
+        public static string path = Directory.GetCurrentDirectory() + "\\banco.sqlite";        // 'static'diz que o ue está sendo criado pertence a classe e 'path' armazenará o local onde sera guardado os dados da aplicação (Banco de dados)
+        private static SQLiteConnection sQLiteConnection;       // conexão com o banco de dados
+
+        private static SQLiteConnection Dbconnection() {        // método que fará a 'conexão' com o banco de dados
+            return new SQLiteConnection(path);
+        }
+
+        public static void CriarBancoSQLite() {      // método que 'criará' o banco de dados caso não exista e por ser 'void' não retorna nada
+
+        }
+
+        public static void CriarTabelaSQLite() {        // método que criará a tabela do banco de dados caso não exista
+
+        }
+
+        public static DataTable GetContatos() {     // método que pegará todos os contatos que estão 'armazenados' no banco de dados
+            return new DataTable();
+        }
+
+        public static DataTable GetContato(int id) {      // método com código do contato 'int id' que 'localizara' um contato
+            return new DataTable();
+        }
+
+        public static void Add(Contato contato) {       // método para adiçionar um contato
+
+        }
+
+        public static void Uodate(Contato contato) {       // método para atualizará um contato
+
+        }
+
+        public static void Delete(int id) {       // método para deletará um contato
+
+        }
+    }
+}
